96fb727556ec680b4a77b451e9d1c5d6ff2a0ea5
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 <Vivekpatil254@gmail.com>
2022-05-23 15:58:16 +0530

    UC3 commit

--- src/com/bridgelabz/EmployeeWageComputationMain.java
+++ src/com/bridgelabz/EmployeeWageComputationMain.java
@@ -3,14 +3,14 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageComputationMain {
-    int wagePerHour=20, fullDayHours=8,wagePerDay=0;
+    int wagePerHour=20, fullDayHours=8,partTimeHours= 4, wagePerDay=0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program ...");
         EmployeeWageComputationMain employee1 =new EmployeeWageComputationMain();
         Random rd = new Random();
 
-        int choice = rd.nextInt(2);
+        int choice = rd.nextInt(3);
 
 
         switch (choice) {
@@ -23,6 +23,10 @@ public class EmployeeWageComputationMain {
                 System.out.println("employee is absent");
                 System.out.println("The employee wage is : 0 ");
                 break;
+            case 2:
+                System.out.println("employee is part time worker");
+                employee1.wagePerDay= employee1.wagePerHour* employee1.partTimeHours;
+                System.out.println("The employee wage is : "+employee1.wagePerDay);
         }
     }
 }
